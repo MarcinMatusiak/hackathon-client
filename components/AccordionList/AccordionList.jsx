6d9757b02f08d052d4a5a4f65f0fc64ebfb16301
Piotr Pośpiech <ptkpospiech@gmail.com>
2019-05-25 05:38:34 +0200

    Create different accordion lists

--- components/AccordionList/AccordionList.jsx
+++ components/AccordionList/AccordionList.jsx
@@ -4,11 +4,15 @@ import { connect } from 'react-redux';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { ExpansionPanelDetails } from '@material-ui/core';
+import { func, string, array } from 'prop-types';
 import Card from '../Card';
 import MessageArea from '../MessageArea';
-import { MyGridList, MyExpansionPanelDetails, MyGridListTile } from './AccordionList';
+import { MyHeader, MyGridList, MyGridListTile } from './AccordionList';
 import { fetchWords } from '../../store/actions';
 import verbSelector from '../../store/selectors/verbWords';
+import nounSelector from '../../store/selectors/nounWords';
+import adjectiveSelector from '../../store/selectors/adjectiveWords';
 
 class AccordionList extends React.PureComponent {
   componentDidMount() {
@@ -27,7 +31,6 @@ class AccordionList extends React.PureComponent {
   }
 
   render() {
-    console.log(this.props.verbs);
     return (
       <div>
         <ExpansionPanel>
@@ -36,10 +39,22 @@ class AccordionList extends React.PureComponent {
               src="http://files.softicons.com/download/system-icons/windows-8-metro-invert-icons-by-dakirby309/png/256x256/Other/Power%20-%20Shut%20Down.png"
               alt="category icon"
             />
+            <MyHeader>{this.props.category}</MyHeader>
           </ExpansionPanelSummary>
-          <MyExpansionPanelDetails>
-            <MyGridList>{this.renderCards(this.props.words)}</MyGridList>
-          </MyExpansionPanelDetails>
+          <ExpansionPanelDetails>
+            {(() => {
+              switch (this.props.category) {
+                case 'Verbs':
+                  return <MyGridList>{this.renderCards(this.props.verbs)}</MyGridList>;
+                case 'Adjectives':
+                  return <MyGridList>{this.renderCards(this.props.adjectives)}</MyGridList>;
+                case 'Nouns':
+                  return <MyGridList>{this.renderCards(this.props.nouns)}</MyGridList>;
+                default:
+                  return null;
+              }
+            })()}
+          </ExpansionPanelDetails>
         </ExpansionPanel>
         <MessageArea />
       </div>
@@ -47,10 +62,20 @@ class AccordionList extends React.PureComponent {
   }
 }
 
+AccordionList.propTypes = {
+  fetchWords: func,
+  category: string,
+  verbs: array,
+  adjectives: array,
+  nouns: array,
+};
+
 export default connect(
   state => ({
     words: state.words,
     verbs: verbSelector(state),
+    nouns: nounSelector(state),
+    adjectives: adjectiveSelector(state),
   }),
   { fetchWords },
 )(AccordionList);
--- components/styles/GlobalStyles.js
+++ components/styles/GlobalStyles.js
@@ -6,6 +6,8 @@ const GlobalStyles = createGlobalStyle`
   margin: 0;
   padding: 0;
   scroll-behavior: smooth;
+  @import url('https://fonts.googleapis.com/css?family=Ubuntu&display=swap');
+  font-family: 'Ubuntu', sans-serif;
 }
 `;
 
